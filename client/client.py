2ae35606ea46d521b48e715a18f891858c2f46ec
mikolajadamski <mikolaj.adamski47@gmail.com>
2020-10-16 20:53:43 +0200

    client final

--- client/client.py
+++ client/client.py
@@ -1,5 +1,7 @@
 import os
 import socket
+import math
+
 """
 REQUESTS
 """
@@ -17,14 +19,26 @@ END_SEND_FILE = '\x0b'
 LOGIN = '\x0c'
 LOGOUT = '\x0d'
 PUBLIC = '\x0e'
+ADD_USER = '\x0f'
+DEL_USER = '\x10'
+SET_PRIVILAGES = '\x11'
+GET_LOGGED = '\x12'
+FORCE_LOGOUT ='\x13'
 
 RESP_OK = '\x00'
 RESP_ERR = '\x01'
 RESP_MORE = '\x02'
 RESP_AUTH_ERR = '\x03'
 RESP_NO_PATH = '\x04'
+RESP_NOT_LOGGED	= '\x05'
+RESP_BUSY = '\x06'
+RESP_NAME_TAKEN = '\x07'
+RESP_NOT_FOUND = '\x08'
 
-
+#privileges:
+GET = 100
+SEND = 10
+DELETE = 1
 class Client:
 
     def __init__(self, user=''):
@@ -56,7 +70,7 @@ class Client:
         it = 0
         recv_packet = None
         try:
-            self.socket.settimeout(0.1)
+            self.socket.settimeout(0.3)
             packet, address = self.socket.recvfrom(1024)
         except socket.timeout:
             pass
@@ -68,14 +82,11 @@ class Client:
                 if(type(data) == str):
                     data = data.encode('utf-8')
                 packet = header + data
-                print(packet)
                 self.socket.sendto(packet, self.SERVER_ADDRESS)
-                self.socket.settimeout(0.2)
+                self.socket.settimeout(0.3)
                 packet, address = self.socket.recvfrom(1024)
                 if address != self.SERVER_ADDRESS:
                     continue
-                if len(packet)>4:
-                    print(packet[1])
                 return packet
             except socket.timeout:
                 it += 1
@@ -93,13 +104,11 @@ class Client:
             if len(packet) == 0:
                 return
             data = packet[self.HEADER_SIZE:].decode('utf-8')
-            packet = packet[:3].decode('utf-8')
-            if packet[0] == GET_FILE_INFO and packet[1] == RESP_OK:
+            result = self.check_data(GET_FILE_INFO,packet)
+            if result == 0:
                 files = data.split(sep='*')
                 for file in files:
                     print(file)
-            if packet[1] != RESP_OK:
-                print("Server returned error")
         except UnicodeDecodeError:
             print("Decode error")
             print(packet)
@@ -113,17 +122,6 @@ class Client:
 
 
     def change_remote_directory(self, path='/'):
-        if path == '/' and self.USER == 'admin0':
-            self.remote_dir_path = '/'
-            return
-        elif path == '/' and self.USER == 'public':
-            self.remote_dir_path = self.public_dir_dir
-            print("Access denied.")
-            return
-        elif path == '/':
-            print("Access denied.")
-            self.remote_dir_path = self.home_dir
-            return
 
         if path == '..':
             if self.remote_dir_path != self.home_dir and self.remote_dir_path != self.public_dir:
@@ -137,61 +135,70 @@ class Client:
                 print("Access denied!")
                 return
 
-        print("sysys")
         if path[0] != '/':
             if self.remote_dir_path == '/':
                 path = self.remote_dir_path + path
             else:
                 path = self.remote_dir_path + '/' + path
         packet = self.send_and_recv_packet(CHANGE_DIRECTORY, path)
-        data = packet[5:].decode('utf-8')
-        packet = packet[:3].decode('utf-8')
-        if packet[0] == CHANGE_DIRECTORY and packet[1] == RESP_OK:
+        result = self.check_data(CHANGE_DIRECTORY, packet)
+        if result == 0:
             self.remote_dir_path = path
             print(self.remote_dir_path)
-        elif packet[1] == CHANGE_DIRECTORY and packet[1] == RESP_NO_PATH:
-            print("Server: Invalid path")
-        else:
-            print("Network Error... Try again")
 
     def send_file(self, filename):
         try:
             op_type = 'rb'
             if filename[-4:] == '.txt':
                 op_type = 'r'
-            with open(filename, op_type) as file:
+            with open(filename, op_type, encoding='utf-8') as file:
+                size = os.stat(filename).st_size
+                counter = 1
                 if self.remote_dir_path == '/':
                     path = self.remote_dir_path + self.USER
                 else:
                     path = self.remote_dir_path + '/' + filename
                 freebuffer = 512 - self.HEADER_SIZE - len(path) - 1
+                estimated_packets = math.ceil(os.stat(filename).st_size/freebuffer)
                 msg_buffer = file.read(freebuffer)
                 if type(msg_buffer) == str:
                     msg_buffer = msg_buffer.encode('utf-8')
                 total_packets_recieved = self.file_control
                 total_packets_recieved = self.iterate_byte(total_packets_recieved)
                 packet = self.send_and_recv_packet(START_SEND_FILE,file_ctrl=total_packets_recieved , data=(path + '*').encode('utf-8') + msg_buffer)
-                packet = packet[:3].decode('utf-8')
+                packet = packet[:2].decode('utf-8')
+                counter = 1
                 if packet[0] == START_SEND_FILE and packet[1] == RESP_OK:
                     total_packets_recieved = self.iterate_byte(total_packets_recieved)
+                elif packet[0] == START_SEND_FILE and packet[1] == RESP_AUTH_ERR:
+                    print("Server: Authorization error.")
+                    return
                 else:
                     print("Error during upload...")
                     return
                 msg_buffer = file.read(freebuffer)
-
+                print(" ")
                 while msg_buffer:
                     if type(msg_buffer) == str:
                         msg_buffer = msg_buffer.encode('utf-8')
                     packet = self.send_and_recv_packet(NEXT_SEND_FILE,file_ctrl=total_packets_recieved ,data=(path + '*').encode('utf-8')  + msg_buffer)
-                    packet = packet[:3].decode('utf-8')
+                    packet = packet[:2].decode('utf-8')
                     if packet[0] == NEXT_SEND_FILE and packet[1] == RESP_OK:
                         total_packets_recieved = self.iterate_byte(total_packets_recieved)
+                        counter +=1
                     else:
                         print("Error during upload.")
                         break
+
                     msg_buffer = file.read(freebuffer)
+
+                    if counter % 10:
+                        print('\r', end='')
+                        print(round((counter/estimated_packets)*100),"%", end='')
+
+
             packet = self.send_and_recv_packet(END_SEND_FILE)
-            packet = packet[:3].decode('utf-8')
+            packet = packet[:2].decode('utf-8')
             if packet[1] == RESP_OK:
                 print("File sent")
         except FileNotFoundError:
@@ -206,7 +213,7 @@ class Client:
         op_type = 'a+b'
         if filename[-4:] == '.txt':
                 op_type = 'a+'
-                file = open(filename, op_type, newline='')
+                file = open(filename, op_type, newline='', encoding='utf-8')
         else:
             file = open(filename, op_type)
         path = ''
@@ -216,34 +223,48 @@ class Client:
             path = self.remote_dir_path + '/' + filename
         total_packets_recieved = self.file_control
         packet = self.send_and_recv_packet(GET_FILE, path, file_ctrl=total_packets_recieved)
-        data = packet[5:]
+        data = packet[6:]
         if op_type == 'a+':
             data = data.decode('utf-8')
-        packet = packet[:3].decode('utf-8')
+        packet = packet[:2].decode('utf-8')
+        counter = 1
+        b_recieved = len(data)
+        if packet[1] == RESP_AUTH_ERR:
+            print("Server: Authorization error")
+            return
         if packet[1] == RESP_MORE:
             file.write(data)
-            print(data, end='')
             total_packets_recieved = self.iterate_byte(total_packets_recieved)
             while(packet[1] == RESP_MORE):
                 packet = self.send_and_recv_packet(GET_FILE, path, file_ctrl=total_packets_recieved)
-                data = packet[5:]
+                data = packet[6:]
+                counter += 1
                 if op_type == 'a+':
                     data = data.decode('utf-8')
-                packet = packet[:3].decode('utf-8')
+                packet = packet[:2].decode('utf-8')
+                b_recieved += len(data)
                 if packet[1] == RESP_MORE:
                     file.write(data)
+
                     total_packets_recieved = self.iterate_byte(total_packets_recieved)
                 elif packet[1] == RESP_OK:
                     file.write(data)
+                    print('\r', end='')
+                    print(b_recieved, " B recieved", end='')
                     print("\nFile received")
                     break
                 elif packet[1] == RESP_ERR:
                     print("Error while downloading")
+                if counter % 10:
+                    print('\r', end='')
+                    print(b_recieved," B recieved", end='')
         elif packet[1] == RESP_OK:
             file.write(data)
-            print(data)
-            print("File received, no loop")
+            print('\r', end='')
+            print(b_recieved, " B recieved", end='')
+            print("File received")
             file.close()
+
             return
 
         elif packet[1] == RESP_ERR:
@@ -261,11 +282,10 @@ class Client:
             path = self.remote_dir_path + '/' + filename
         packet = self.send_and_recv_packet(DELETE_FILE, path)
         data = packet[5:].decode('utf-8')
-        packet = packet[:3].decode('utf-8')
-        if packet[0] != DELETE_FILE or packet[1] == RESP_ERR:
-            print("Network Error")
-        else:
+        response = self.check_data(DELETE_FILE, packet)
+        if response == 0:
             print("File has been deleted.")
+
 
     def create_directory(self, dirname):
         path = ''
@@ -275,13 +295,9 @@ class Client:
             path = self.remote_dir_path + '/' + dirname
         packet = self.send_and_recv_packet(CREATE_DIRECTORY, path)
         data = packet[5:].decode('utf-8')
-        packet = packet[:3].decode('utf-8')
-        if packet[0] != CREATE_DIRECTORY or packet[1] == RESP_ERR:
-            print("Network Error")
-        elif packet[1] == RESP_OK:
-            print("Directory has been created.")
-        else:
-            print("Error")
+        result = self.check_data(CREATE_DIRECTORY, packet)
+        if result == 0:
+            print("Directory has been created")
 
     def delete_directory(self, dirname):
         path = ''
@@ -291,11 +307,9 @@ class Client:
             path = self.remote_dir_path + '/' + dirname
         packet = self.send_and_recv_packet(DELETE_DIRECTORY, path)
         data = packet[5:].decode('utf-8')
-        packet = packet[:3].decode('utf-8')
-        if packet[0] != DELETE_DIRECTORY or packet[1] == RESP_ERR:
-            print("Network Error")
-        else:
-            print("Directory has been deleted.")
+        result = self.check_data(CREATE_DIRECTORY, packet)
+        if result == 0:
+            print("Directory created")
 
 
     def iterate_byte(self, byte):
@@ -313,18 +327,15 @@ class Client:
             return
         self.USER = username
         packet = self.send_and_recv_packet(LOGIN, password)
-        print(packet)
         result = self.check_data(LOGIN, packet)
         if result == 0:
             print("Login successful")
-            print(packet[2])
             self.ID = packet[2].to_bytes(1,'big')
             self.home_dir = self.remote_dir_path = '/' + self.USER
 
             return 1
         else:
             self.USER = 'public'
-            print("Err")
             return 0
 
 
@@ -332,11 +343,25 @@ class Client:
         if len(packet)<1:
             return -1
         data = packet[5:].decode('utf-8')
-        packet = packet[:3].decode('utf-8')
-        if packet[0] != op or packet[1] == RESP_ERR:
-            return -1
-        else:
+        packet = packet[:2].decode('utf-8')
+        if packet[0] == op and packet[1] == RESP_OK:
             return 0
+        elif packet[1] == RESP_AUTH_ERR:
+            print("Server: Authorization error")
+            return -1
+        elif packet[1] == RESP_NO_PATH:
+            print("Server: Path does not exist")
+        elif packet[1] == RESP_NOT_LOGGED:
+            print("Server: Not logged in")
+        elif packet[1] == RESP_NAME_TAKEN:
+            print("Server: Name is already taken")
+        elif packet[1] == RESP_NOT_FOUND:
+            print("Server: Name hasn't been found")
+        else:
+            print("Error")
+            return -1
+        return -1
+
 
     def public(self):
         packet = self.send_and_recv_packet(PUBLIC)
@@ -350,10 +375,78 @@ class Client:
 
     def exit(self):
         result = 1
+        if self.USER == 'public':
+            return
         packet = self.send_and_recv_packet(LOGOUT)
-        result = self.check_data(LOGIN, packet)
+        result = self.check_data(LOGOUT, packet)
 
         return 1
+
+
+    def add_user(self):
+        username = input("Username(6 char.): ")
+        if len(username) != 6:
+            print("Wrong username length")
+        passwd = input("Password(8 char.): ")
+        if len(passwd) != 8:
+            print("Wrong password length")
+        packet = self.send_and_recv_packet(ADD_USER,username+passwd)
+        result = self.check_data(ADD_USER, packet)
+        if result == 0:
+            print("User has been registered")
+
+
+    def del_user(self):
+        username = input("Username(6 char.): ")
+        if len(username) != 6:
+            print("Wrong username length")
+        packet = self.send_and_recv_packet(DEL_USER,username)
+        result = self.check_data(DEL_USER, packet)
+        if result == 0:
+            print("User has been unregistered")
+
+    def set_privileges(self):
+        data = input("Set username and privileges(i.e. usr123+SEND+GET+DELETE:\n").split(sep='+')
+        privs = 0
+        username = data[0]
+        privs_list = data[1:]
+        if 'GET' in privs_list:
+            privs += GET
+        if 'SEND' in privs_list:
+            privs += SEND
+        if 'DELETE' in privs_list:
+            privs += DELETE
+        privs = privs.to_bytes(1,'big')
+        print(privs)
+        packet = self.send_and_recv_packet(SET_PRIVILAGES, username.encode('utf-8')+privs)
+        result = self.check_data(SET_PRIVILAGES, packet)
+        if result == 0:
+            print("Privileges changed for",username,"to",privs_list)
+
+    def get_logged(self):
+        packet = self.send_and_recv_packet(GET_LOGGED)
+        result = self.check_data(GET_LOGGED, packet)
+
+        if result == 0:
+            data = packet[6:]
+            print("Logged ID: ",end='')
+            for i in range(len(data)):
+                val = data[i]
+                if val == 0:
+                    break
+                print(val, end=',')
+
+            print(" ")
+
+    def force_logout(self):
+        id = int(input("ID: "))
+        data = id.to_bytes(1,'big')
+        packet = self.send_and_recv_packet(FORCE_LOGOUT, data)
+        result = self.check_data(FORCE_LOGOUT, packet)
+        if result == 0:
+            print("User has been logged out")
+
+
 
 
 if __name__ == '__main__':
@@ -361,12 +454,14 @@ if __name__ == '__main__':
     isLogged = 0;
     while not isLogged:
         command = ''
-        command = input("LOGIN or PUBLIC:").upper()
+        command = input("REGISTER, LOGIN or PUBLIC:").upper()
         if command == 'PUBLIC':
             isLogged = client.public()
         elif command == 'LOGIN':
             username = input("username: ")
             isLogged = client.login(username)
+        elif command == 'REGISTER':
+            client.add_user()
         else:
             print("Command unknown")
 
@@ -409,13 +504,14 @@ if __name__ == '__main__':
                 print("Give filename")
         elif command[0] == 'GET':
             try:
-                print(command[1:])
+
                 client.get_file(" ".join(command[1:]))
+
             except IndexError:
                 print("Give filename")
         elif command[0] == 'DEL':
             try:
-                client.delete_file(command[1])
+                client.delete_file(" ".join(command[1:]))
             except IndexError:
                 print("Give filename")
         elif command[0] == 'ND':
@@ -431,3 +527,16 @@ if __name__ == '__main__':
         elif command[0] == "EXIT":
             end = client.exit()
             break
+
+        elif command[0] == "REGISTER":
+            client.add_user()
+
+        elif command[0] == "UNREGISTER":
+            client.del_user()
+
+        elif command[0] == "PRIVS":
+            client.set_privileges()
+        elif command[0] == "LOGOUT":
+            client.force_logout()
+        elif command[0] == "LOGGED":
+            client.get_logged()
